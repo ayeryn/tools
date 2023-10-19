169b3fb71b3f20d6612db4c345533a46f85d4fdf
ayeryn <erynli@utexas.edu>
2023-10-19 12:17:05 -0500

    Modulize

--- hour_delta.py
+++ hour_delta.py
@@ -1,14 +1,24 @@
 from datetime import timedelta
 
-start_hour = input('Start time? (24H) ')
-start_min = input('Start minute? ')
-delta_hour = input('Delta hour?')
-delta_min = input('Delta minute? ')
 
-start_time = timedelta(hours=int(start_hour),
-                       minutes=int(start_min) if start_min else 0)
-time_delta = timedelta(hours=int(delta_hour),
-                       minutes=int(delta_min) if delta_min else 0)
+def calculate():
+    start_hour = input('Start time? (24H) ')
+    start_min = input('Start minute? ')
+    delta_hour = input('Delta hour? ')
+    delta_min = input('Delta minute? ')
 
-end_time = start_time + time_delta
-print(end_time)
+    start_time = timedelta(hours=int(start_hour),
+                           minutes=int(start_min) if start_min else 0)
+    time_delta = timedelta(hours=int(delta_hour),
+                           minutes=int(delta_min) if delta_min else 0)
+
+    end_time = start_time + time_delta
+    print(end_time)
+
+
+again = True
+while again:
+    calculate()
+    again = input('Run again? (Y/N) ').lower() == 'y'
+
+print('Goodbye!')
